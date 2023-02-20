4b4419052efa687ac9d7577b819e967caf410b11
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-02-21 00:00:30 +0200

    Delete firebaseConfig.js

--- firebase/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBjeYWy1XZW0nzDQrM0zP8pOhAWwuUgL7s",
-  authDomain: "rn-project-ba8b1.firebaseapp.com",
-  projectId: "rn-project-ba8b1",
-  storageBucket: "rn-project-ba8b1.appspot.com",
-  messagingSenderId: "618890077684",
-  appId: "1:618890077684:web:4d5109f99e3cf8221c85a1",
-};
